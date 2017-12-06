b308402460b9013074a180842a27bc3e9d2662aa
Tejas Khatiwala <tejas@cerebras.net>
2017-12-06 15:13:41 -0800

    Auto enable ETH0 at startup with DHCP. Update SSHD config to allow password less root access.

--- br-post-sm-build.sh
+++ br-post-sm-build.sh
@@ -4,11 +4,21 @@
 # │ first argument. The script is executed from the main Buildroot        │
 # │ source directory as the current directory.                            │
 
-OUTPUT=$1
+TARGET=$1
 TOPDIR=$(pwd)
 
 echo "Running Cerebras SM post build script..."
 echo "----------------------------------------"
 
 echo "Copying Cerebras SM skeleton files"
-tar -C $TOPDIR/cerebras/sm-skel -cf - . | (cd $OUTPUT && tar xf -)
+tar -C $TOPDIR/cerebras/sm-skel -cf - . | (cd $TARGET && tar xf -)
+
+echo "Enabling ETH0 at startup"
+echo "auto eth0" >> $TARGET/etc/network/interfaces
+echo "iface eth0 inet dhcp" >> $TARGET/etc/network/interfaces
+
+echo "Enabling SSH root access without password"
+sed -i 's/^#PermitRootLogin prohibit-password/PermitRootLogin yes/' \
+    $TARGET/etc/ssh/sshd_config
+sed -i 's/^#PermitEmptyPasswords no/PermitEmptyPasswords yes/' \
+    $TARGET/etc/ssh/sshd_config
