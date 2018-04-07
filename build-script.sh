c68426eac02419b1ca105ce45917cf4c80e1ff5d
Tejas Khatiwala <tejas@cerebras.net>
2018-04-06 22:17:51 -0700

    Added toolchain for __arm__
    Started out with cerebras,buildroot_sm.config and changed its toolchain
    settings. Pointed it to existing toolchain downloaded from linaro:
    
    link: https://releases.linaro.org/components/toolchain/binaries/5.2-2015.11-2/arm-linux-gnueabi/
    
    Saved on:
    builder:/home/tejas/toolchain/gcc-linaro-5.2-2015.11-2-x86_64_arm-linux-gnueabi
    builder:/home/tejas/toolchain/sysroot-linaro-glibc-gcc5.2-2015.11-2-arm-linux-gnueabi
    
    Along with that ARCH and EABI settings update. Built the buildroot. Had to
    remove Python support cause it got stuck there.
    
    REASON why this exercise because buildroot builds additional header files and
    libraries (libusb, libudev, zlib, etc.) that's not there in vanilla toolchain
    downloaded from linaro website.
    
    Little patch needed in build-script.sh. Installed under /cb/platform with
    following command:
    
    ```
    export LOCAL_BR_TOOLCHAIN_DIR=/home/tejas/toolchain/gcc-linaro-5.2-2015.11-2-x86_64_arm-linux-gnueabi
    export LOCAL_BR_SYSROOT_DIR=output/host/usr/arm-buildroot-linux-gnueabi/sysroot
    export REMOTE_BUILD_HOST_HOME=/cb/platform/buildtools/x86_64-tools/arm
    sh build-script.sh
    ```

--- build-script.sh
+++ build-script.sh
@@ -4,10 +4,10 @@ SYNCCMD="rsync -avz --delete"
 # BUILD_HOST=server3.cerebras.aws
 BUILD_HOST=${BUILD_HOST:-builder}
 
-LOCAL_BR_TOOLCHAIN_DIR=output/host/opt/ext-toolchain
-LOCAL_BR_SYSROOT_DIR=output/host/usr/aarch64-buildroot-linux-gnu/sysroot
+LOCAL_BR_TOOLCHAIN_DIR=${LOCAL_BR_TOOLCHAIN_DIR:-output/host/opt/ext-toolchain}
+LOCAL_BR_SYSROOT_DIR=${LOCAL_BR_SYSROOT_DIR:-output/host/usr/aarch64-buildroot-linux-gnu/sysroot}
 
-REMOTE_BUILD_HOST_HOME=/cb/platform/buildtools/x86_64-tools/aarch64
+REMOTE_BUILD_HOST_HOME=${REMOTE_BUILD_HOST_HOME:-/cb/platform/buildtools/x86_64-tools/aarch64}
 LINARO_VER=
 
 fail() { echo $* >&2; exit 1; }
@@ -16,11 +16,13 @@ warn() { echo $* >&2; return 1; }
 find_linaro_ver()
 {
     # gcc-linaro-6.4.1-2017.08-linux-manifest.txt
-    local manifest=$(ls $LOCAL_BR_TOOLCHAIN_DIR/gcc-*-linux-manifest.txt 2>/dev/null)
+    local manifest=$(ls $LOCAL_BR_TOOLCHAIN_DIR/gcc-*-manifest.txt 2>/dev/null \
+        | sed 's/-linux//g')
 
     [ -z "$mainfest" ] || fail "linaro toolchain mainfest file doesn't exists"
 
-    LINARO_VER=$(echo $manifest | sed 's/^.*gcc-\(.*\)-linux-manifest.txt$/\1/')
+    LINARO_VER=$(echo $manifest | sed 's/^.*gcc-\(.*\)-manifest.txt$/\1/' | \
+        sed 's/-linux//g')
     [ -z "$LINARO_VER" ] && \
         fail "failed to extract linaro version from $manifest"
 
